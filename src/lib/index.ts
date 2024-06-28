2486ff54c4e340f3fc257fdd17ed79bbd1db4478
lasse <77295879+lassejlv@users.noreply.github.com>
2024-06-28 08:26:36 +0200

    what

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
-// export const API_URL = "https://svelte-thing.coolify-server.lassejlv.dk/api";
-export const API_URL = "http://localhost:5001/api";
+export const API_URL = "https://svelte-thing.coolify-server.lassejlv.dk/api";
+// export const API_URL = "http://localhost:5001/api";
 export const GITHUB = "https://github.com/lassejlv/vite-svelete-learning";
